062d68e566d9b88e6f7e317aee7de2d587572b03
VincentLong73 <longngodaugo.1202@gmail.com>
2020-12-24 13:13:59 +0700

    crossOrigin

--- src/main/java/com/soict/reviewshopfood/controller/AdminController.java
+++ src/main/java/com/soict/reviewshopfood/controller/AdminController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,6 +24,7 @@ import com.soict.reviewshopfood.service.impl.UserService;
 
 @RestController
 @RequestMapping(value="/api/admin")
+@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
 public class AdminController {
 	
 	@Autowired
